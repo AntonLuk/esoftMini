67874c1b61b56d0e328140b7e6e193e6dbf53a10
a.v.lukyanov <antonluk08522@gmail.com>
2023-11-01 22:21:20 +0500

    init4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(null)
+  const [count, setCount] = useState(0)
 
-  return (
+
+    return (
     <>
       <div>
         {/*<a href="https://vitejs.dev" target="_blank">*/}
@@ -26,7 +25,7 @@ function App() {
             </button>
         </div>
         <div className="card">
-            <h2>{count}</h2>
+            <h2>{!!count && count}</h2>
         </div>
     </>
   )
